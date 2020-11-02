5289afa5a6f1861a6a79ec84f022878871166f65
neryad <dayern.gomez@hotmail.com>
2020-11-01 23:56:05 -0400

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { TrainingComponent } from './components/training/training.component';
+import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
+import { NewTrainingComponent } from './components/training/new-training/new-training.component';
+import { PastTrainingComponent } from './components/training/past-training/past-training.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
