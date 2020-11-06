411fee4764563a0ac251c31db1e56137bf0fc55e
neryad <dayern.gomez@hotmail.com>
2020-11-06 19:09:59 -0400

    adding sorting

--- src/app/components/training/past-training/past-training.component.ts
+++ src/app/components/training/past-training/past-training.component.ts
@@ -1,20 +1,27 @@
 import { TrainingService } from './../../../services/trainig.service';
 import { Exercise } from './../../../models/exercie.model';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css'],
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   dislayColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private trainigService: TrainingService) {}
 
   ngOnInit(): void {
     this.dataSource.data = this.trainigService.getCompleteOrcancelExeciers();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
   }
 }
